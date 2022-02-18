4beed18a9af6c3905e23d6b50af4473c95a39abd
Fabricio Vaz <vazmonteirofabricio@gmail.com>
2022-02-17 22:51:06 -0500

    corrigindo o codico 3

--- test.py
+++ test.py
@@ -19,7 +19,7 @@ class Test(unittest.TestCase):
         result = self.app.get('/') 
 
         # verifica o retorno do conteudo da pagina
-        self.assertRegex(result.data.decode(), "Pipeline DevOps - Fabricio Vaz2")
+        self.assertRegex(result.data.decode(), "Pipeline DevOps - Fabricio Vaz")
 
 
 if __name__ == "__main__":
